889e4658074037f2d972aebee19d7c67296d3d86
ShneiderOrtiz <shneiderortiz20@gmail.com>
2025-02-20 10:00:54 -0500

    Delete Tarea Python by Shneider Ortiz for Brayantorres2 directory

--- Tarea Python by Shneider Ortiz for Brayantorres2/Ejercicio2.py
+++
@@ -1,8 +0,0 @@
-#Solicitar capital
-capital=float(input( "Ingresar capital que se invierte: "))
-#Calcular interes y monta final
-interes = capital * 0.02
-monto_final = capital + interes
-#Resultado
-print(f"El interes ganado en un mes quivale a: {interes:.2f} COP")
-print(f"El monto final despues de un mes será: {monto_final:.2f} COP")
